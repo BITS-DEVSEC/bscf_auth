06a1145793e3ca1e36cc8cba8bd69cd5304a4aaf
Asrat <asratextras77@gmail.com>
2025-03-31 10:20:29 +0300

    Fix rspec configs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,14 @@
 require "simplecov"
-#require "./spec/support/models/shared_examples"
-#require "./spec/support/requests/shared_requests"
-require "rspec/retry"
+# require "./spec/support/models/shared_examples"
+# require "./spec/support/requests/shared_requests"
+#require "rspec/retry"
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
+
+  config.formatter = RSpec::Core::Formatters::DocumentationFormatter
 
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
@@ -15,15 +17,12 @@ RSpec.configure do |config|
   config.shared_context_metadata_behavior = :apply_to_host_groups
   config.example_status_persistence_file_path = "spec/examples.txt"
   config.disable_monkey_patching!
-
-  config.formatter = RSpec::Core::Formatters::DocumentationFormatter
-
   config.profile_examples = 10
   config.order = :random
   Kernel.srand config.seed
 
-  config.verbose_retry = true
-  config.display_try_failure_messages = true
+#  config.verbose_retry = true
+  #config.display_try_failure_messages = true
   config.around :each, :js do |ex|
     ex.run_with_retry retry: 3
   end
