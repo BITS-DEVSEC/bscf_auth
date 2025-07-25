67fccee6b5fbbae0841dade320fd230b28afe0a0
Asrat <asratextras77@gmail.com>
2025-07-25 11:26:50 +0300

    fix(user_profiles): Handle RecordNotFound and authorize user

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -1,10 +1,10 @@
 class UserProfilesController < ApplicationController
   before_action :is_authenticated
-  before_action :is_allowed, only: [ :update_kyc ]
   before_action :set_user_profile, only: [ :show ]
 
   def show
     if @user_profile
+      authorize @user_profile
       render json: {
         success: true,
         user_profile: @user_profile.as_json(include: :address)
@@ -18,11 +18,12 @@ class UserProfilesController < ApplicationController
   end
 
   def update_kyc
-    return render json: { success: false, error: "KYC status is required" },
-                status: :unprocessable_entity unless params[:kyc_status].present?
-
     begin
       user_profile = Bscf::Core::UserProfile.find(params[:id])
+      authorize user_profile
+
+      return render json: { success: false, error: "KYC status is required" },
+                  status: :unprocessable_entity unless params[:kyc_status].present?
 
       if user_profile.update(
         kyc_status: params[:kyc_status],
@@ -43,10 +44,5 @@ class UserProfilesController < ApplicationController
 
   def set_user_profile
     @user_profile = current_user.user_profile
-  end
-
-  def is_allowed
-    user_role = current_user.user_roles.find { |ur| ur.role.name == "Admin" }
-    render json: { success: false, error: "Not authorized" }, status: :unauthorized unless user_role
   end
 end
